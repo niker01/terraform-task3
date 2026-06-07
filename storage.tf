43f177ddf77e19536247d59d0a7f52cf49d18842
Nik Niker <niker234567@gmail.com>
2026-06-07 15:22:32 +0300

    a

--- storage.tf
+++ storage.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "bucket_block" {
+resource "aws_s3_bucket_public_access_block" "block" {
   bucket = aws_s3_bucket.bucket.id
 
   block_public_acls       = true
